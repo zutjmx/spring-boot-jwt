05728be943eb280107ceb38df931d30aa4798846
Jesús Zúñiga Trejo <zutjmx@gmail.com>
2021-10-04 18:39:33 -0500

    Modificaciones 04/10/2021

--- src/main/java/com/zutjmx/springboot/app/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/zutjmx/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -1,5 +1,12 @@
 package com.zutjmx.springboot.app.auth.filter;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,8 +14,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -47,6 +61,33 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username,password); 
 		
 		return authenticationManager.authenticate(authToken);
+		
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		
+		String username = ((User) authResult.getPrincipal()).getUsername();
+		
+		SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+		
+		String token = Jwts.builder()
+				.setSubject(username)
+				//.signWith(SignatureAlgorithm.HS512, "Alguna.Clave.Secreta.123456".getBytes())
+				.signWith(secretKey)
+				.compact();
+		
+		response.addHeader("Authorization", "Bearer ".concat(token));
+		
+		Map<String, Object> body = new HashMap<String, Object>();
+		body.put("token", token);
+		body.put("user", (User) authResult.getPrincipal());
+		body.put("mensaje", String.format("¡Se ha iniciado sesión con éxito con el usuario %s!", username));
+		
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(200);
+		response.setContentType("application/json");
 		
 	}
 
